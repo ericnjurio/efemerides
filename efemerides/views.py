7df4d825c51cdaf9fe9d5948f8be9ba879e571de
Eric Jurio <ericnahueljurio@gmail.com>
2020-08-04 18:44:48 -0300

    Add swagger in efemerides commemoration_detail view.

--- efemerides/views.py
+++ efemerides/views.py
@@ -2,9 +2,24 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 from .serializers import CommemorationSerializer
 
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Get commemorations details.",
+    manual_parameters=(
+        openapi.Parameter(
+            'day',
+            openapi.IN_QUERY,
+            description="'day' filter",
+            type=openapi.TYPE_STRING
+        ),
+    )
+)
 @api_view(['GET'])
 def commemoration_detail(request):
     """Get commemorations details."""
